269fe98e633d2891cbbc1edfcfced70fc08c4df9
geonhee-youha <geonhee.lee@youha.info>
2022-10-27 17:19:00 +0900

    add index to router push home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import _ from "lodash";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/home");
+  }, []);
   return <></>;
 }
