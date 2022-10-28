c73221c4c38c804b38270bf764ddedd743fefebd
geonhee-youha <geonhee.lee@youha.info>
2022-10-28 16:33:21 +0900

    드로어/팝업 진행중

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -2,9 +2,34 @@ import { atom } from "recoil";
 export type DrawerProps = {
   open: boolean;
 }
-export const alarmDrawerState = atom<DrawerProps>({
-  key: "Drawer/alarmDrawerState",
+export const searchDrawerState = atom<DrawerProps>({
+  key: "searchDrawerState",
   default: {
     open: false
   },
 });
+export const alarmDrawerState = atom<DrawerProps>({
+  key: "alarmDrawerState",
+  default: {
+    open: false
+  },
+});
+export const campaignDrawerState = atom<DrawerProps>({
+  key: "campaignDrawerState",
+  default: {
+    open: false
+  },
+});
+export const adDrawerState = atom<DrawerProps>({
+  key: "adDrawerState",
+  default: {
+    open: false
+  },
+});
+export const estimateDrawerState = atom<DrawerProps>({
+  key: "estimateDrawerState",
+  default: {
+    open: false
+  },
+});
+export const alertPopupState
